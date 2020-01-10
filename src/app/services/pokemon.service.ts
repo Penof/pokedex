d2219548935cff5f486422cce1a8d2b143d58a87
adrien.grainville@gmail.com <ellivniarg0>
2020-01-10 08:16:07 +0100

    import

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -2,9 +2,10 @@ import { PagedData } from './../models/paged-data.model';
 import { Pokemon } from '../models/pokemon.model';
 import { Identifiants } from '../models/identifiants.model';
 import { Injectable } from '@angular/core';
-import { of, Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { of, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { DetailPokemon } from '../models/detail-pokemon.model';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -47,6 +48,31 @@ export class PokemonService {
       headers: new HttpHeaders({ 'Content-Type': 'application/json', Authorization: 'Bearer ' + localStorage.getItem('access_token')})
     };
     const url = this.pokemonUrl + '/trainers/me/team';
-    return this.httpClient.get<number[]>(url, httpOptions);
+    return this.httpClient.get<number[]>(url, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  refreshToken() {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+    };
+    return this.httpClient.post(this.pokemonUrl + '/auth/refresh', localStorage.getItem('refresh_token'), httpOptions);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
